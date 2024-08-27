7dea2a39918241ac2769466a77c5e5d90bff5a74
Kevin Tran <kevintran11082002@gmail.com>
2024-08-27 14:42:26 -0400

    finsihed logic for player choices

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var humanScore,computerScore;
+humanScore = computerScore = 0;
 
 function getComputerChoice(){
     const choice = ['rock', 'paper', 'scissors'];
@@ -7,3 +9,16 @@ function getComputerChoice(){
 // for(let i = 0; i < 5; i++){
 //     console.log(getComputerChoice());
 // }
+
+function getHumanChoice(){
+  let answer = prompt('Enter your choice: rock, paper or scissors');
+    if(answer.toLowerCase() === 'rock' || answer.toLowerCase() === 'paper' || answer.toLowerCase() === 'scissors'){
+        answer = answer.toLowerCase();
+        return answer;
+    }else{
+        alert('Please enter rock, paper or scissors ONLY!');
+        return getHumanChoice();
+    }
+}
+
+getHumanChoice();
